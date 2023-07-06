835177087214122838759bae78b265e095ed459a
Radu-A <elcastravete@gmail.com>
2023-07-06 22:24:47 +0200

    rearranged/components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import List from "./List/List";
+import LogoBig from '../../assets/images/logo-big.png';
+import SearchForm from "./SearchForm/SearchForm";
 
 const Home = () => {
-  return <div>Home</div>;
+
+  const [list, setList] = useState([])
+
+  return (
+    <>
+      <section className="title-section">
+        <img className="logo-big" src={LogoBig} alt="" />
+        <h1>App</h1>
+      </section>
+      <SearchForm list={list} setList={setList} />
+      <List list={list}/>
+    </>
+  );
 };
 
 export default Home;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,25 +1,14 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Routes, Route } from 'react-router-dom';
-import List from "./List/List";
-import LogoBig from '../../assets/images/logo-big.png';
-import SearchForm from "./SearchForm/SearchForm";
 import Home from "../Home/Home";
 import New from "../New/New";
 import Details from "../Details/Details";
 
 const Main = () => {
 
-  const [list, setList] = useState([])
-
   return (
     <>
       <main>
-        {/* <section className="title-section">
-          <img className="logo-big" src={LogoBig} alt="" />
-          <h1>App</h1>
-        </section>
-        <SearchForm list={list} setList={setList} />
-        <List list={list}/> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
